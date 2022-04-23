e3ed34414b40c523946400f801b391a1d0ac45f2
taku1219 <acmiran189926@gmail.com>
2022-04-23 20:13:59 +0900

    [add] Template_useTodoStore

---
+++ src/data/stores/useToDoStore.ts
@@ -0,0 +1,21 @@
+import create from 'zustand';
+
+interface Task {
+  id: string;
+  title: string;
+  createdAt: number;
+}
+
+interface ToDoStore {
+  tasks: Task[];
+  createTask: (title: string) => void;
+  updateTask: (id: string, title: string) => void;
+  removeTask: (id: string) => void;
+}
+
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+  tasks: [],
+  createTask: (title) => {},
+  updateTask: (id, title) => {},
+  removeTask: (id) => {},
+}));
