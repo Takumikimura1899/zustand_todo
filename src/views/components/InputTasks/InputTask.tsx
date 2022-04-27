b06719db37b48ecf1c750fd1ba7b115ff625d686
taku1219 <acmiran189926@gmail.com>
2022-04-27 10:04:13 +0900

    [add] components/inputTask checkbox,title,remove&trashbotton

--- src/views/components/InputTasks/InputTask.tsx
+++ src/views/components/InputTasks/InputTask.tsx
@@ -20,5 +20,37 @@ export const InputTask: React.FC<InputTaskProps> = ({
   onEdited,
   onRemoved,
 }) => {
-  return <div className={styles.inputTask}>{title}</div>;
+  const [checked, setChecked] = useState(false);
+  return (
+    <div className={styles.inputTask}>
+      <label className={styles.inputTaskLabel} htmlFor=''>
+        <input
+          type='checkbox'
+          checked={checked}
+          className={styles.inputTaskCheckbox}
+          onChange={(e) => {
+            setChecked(e.target.checked);
+            if (e.target.checked) {
+              onDone(id);
+            }
+          }}
+        />
+        <h3 className={styles.inputTaskTitle}>{title}</h3>
+      </label>
+      <button
+        aria-label='Remove'
+        className={styles.inputTaskRemove}
+        onClick={() => {}}
+      />
+      <button
+        aria-label='Edit'
+        className={styles.inputTaskEdit}
+        onClick={() => {
+          if (confirm('本当に削除しても良いですか？')) {
+            onRemoved(id);
+          }
+        }}
+      />
+    </div>
+  );
 };
